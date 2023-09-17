37ff7a18515ee8a1e169d9920c47c0083b31d412
Kishan Kumar <hskishanraj@gmail.com>
2023-09-17 23:51:45 +0530

    new change

--- Client/src/components/global/Navbar.jsx
+++ Client/src/components/global/Navbar.jsx
@@ -1,13 +1,15 @@
 import "./Navbar.css"
-import {HomeFilled, SearchOutlined,AppstoreAddOutlined,PlusOutlined,GlobalOutlined} from "@ant-design/icons"
-import {IconButton} from "@mui/material"
+import { HomeFilled, SearchOutlined, AppstoreAddOutlined, PlusOutlined, GlobalOutlined } from "@ant-design/icons"
+import { IconButton } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
-    return(
+    const navigate = useNavigate()
+    return (
         <div className="navbar">
             <div className="navigation">
                 <ul className="nav-list">
-                    <li className="nav-items">
+                    <li className="nav-items" onClick={() => navigate('/')}>
                         <HomeFilled />
                         <span>Home</span>
                     </li>
@@ -19,20 +21,20 @@ const Navbar = () => {
             </div>
             <div className="your-library-container">
                 <div className="library-top-section ">
-                    
-                        <AppstoreAddOutlined style={
-                            {fontSize:"20"}
-                        }/>
+
+                    <AppstoreAddOutlined style={
+                        { fontSize: "20" }
+                    } />
                     <span>your library</span>
-                    
-                    
-                        <IconButton style={{
-                            marginLeft: "auto"
-                        }}>
-                            <PlusOutlined style={{ color: "#fff" }} />
-                        </IconButton>
-                    
-                    
+
+
+                    <IconButton style={{
+                        marginLeft: "auto"
+                    }}>
+                        <PlusOutlined style={{ color: "#fff" }} />
+                    </IconButton>
+
+
                 </div>
                 <div className="library-middle-setion">
                     <div className="create-playlist-container">
@@ -47,23 +49,23 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className="navbar-footer-setion">
-                <div className="nav-footer-links">
-                    <p className="nav-link">legal</p>
-                    <p className="nav-link">privacy center</p>
-                    <p className="nav-link">privacy policy</p>
-                    <p className="nav-link">cookies</p>
-                    <p className="nav-link">about ads</p>
-                    <p className="nav-link">accessibility</p>
-                </div>
-                
-                <button>
-                    <GlobalOutlined/>
-                    english
+                    <div className="nav-footer-links">
+                        <p className="nav-link">legal</p>
+                        <p className="nav-link">privacy center</p>
+                        <p className="nav-link">privacy policy</p>
+                        <p className="nav-link">cookies</p>
+                        <p className="nav-link">about ads</p>
+                        <p className="nav-link">accessibility</p>
+                    </div>
+
+                    <button>
+                        <GlobalOutlined />
+                        english
                     </button>
+                </div>
             </div>
-            </div>
-            
-            
+
+
         </div>
     )
 }
